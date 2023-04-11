847df0f3a3a4313edebd94fc13b555c153244447
Qiaozp <qiaozhongpei.qzp@alibaba-inc.com>
2023-04-11 16:29:02 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# velad-bot
+
+> A GitHub App built with [Probot](https://probot.github.io) that a bot for velad
+
+## Setup
+
+```sh
+# Install dependencies
+npm install
+
+# Run the bot
+npm start
+```
+
+## Contributing
+
+If you have suggestions for how velad-bot could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
+
+For more, check out the [Contributing Guide](CONTRIBUTING.md).
+
+## License
+
+[ISC](LICENSE) © 2023 chivalryq
---
+++ index.js
@@ -0,0 +1,105 @@
+/**
+ * This is the entry point for your Probot App.
+ * @param {import('probot').Application} app - Probot's Application class.
+ */
+const { exec } = require('child_process')
+const path = require('path')
+const fs = require('fs')
+const simpleGit = require('simple-git')
+const os = require('os')
+const util = require('util')
+const { Octokit, RestEndpointMethodTypes } = require('@octokit/rest')
+const { createAppAuth } = require('@octokit/auth-app');
+
+const execAsync = util.promisify(exec)
+
+// eslint-disable-next-line no-unused-vars
+const velaRepo = 'kubevela/kubevela'
+const testRepo = 'chivalryq/test-matrix-on-tags'
+// eslint-disable-next-line no-unused-vars
+const catalogRepo = 'kubevela/catalog'
+var watchRepo = testRepo
+
+module.exports = app => {
+  app.on(['release.published'], async context => {
+    if (context.payload.repository.full_name === watchRepo) {
+      const tagName = context.payload.release.tag_name
+      const prTitle = 'Bump kubevela version to ' + tagName
+      const prBody = 'Update kubevela/velad'
+
+      // Clone the velad repository
+      const veladRepo = 'https://github.com/kubevela/velad.git'
+      const tmpParentDir = path.join(os.tmpdir(), 'velad')
+      if (!fs.existsSync(tmpParentDir)) {
+        fs.mkdirSync(tmpParentDir)
+      }
+      const repoTmpDir = fs.mkdtempSync(path.join(tmpParentDir, 'velad-'))
+      const repoPath = path.join(repoTmpDir, 'velad')
+      const git = simpleGit(repoTmpDir)
+
+      try {
+        await git.clone(veladRepo, repoPath)
+        app.log.info('Cloning ' + veladRepo + ' to ' + repoPath)
+
+        // Execute the upgrade script
+        const upgradeScript = path.join(repoPath, 'hack', 'upgrade_vela.sh')
+        try {
+          const { stdout, stderr } = await execAsync(`${upgradeScript} ${tagName}`, { cwd: repoPath })
+          console.log(`stdout: ${stdout}`)
+          console.error(`stderr: ${stderr}`)
+        } catch (error) {
+          console.error(`exec error: ${error}`)
+          return
+        }
+
+        // Create a new branch and commit changes
+        const newBranch = `velad-bot/bump-kubevela-version-${tagName}`
+        const gitRepo = simpleGit(repoPath)
+        await gitRepo.checkoutLocalBranch(newBranch)
+        await gitRepo.addConfig('user.name', 'velad-bot')
+        await gitRepo.addConfig('user.email', 'chivalry.pp@gmail.com')
+        await gitRepo.add('./*')
+        await gitRepo.commit(`Update kubevela version to ${tagName}`, { '--signoff': null })
+
+        // Push the new branch to the remote repository
+        const gitToken = process.env.GITHUB_TOKEN
+        const remoteUrlWithToken = veladRepo.replace('https://', `https://${gitToken}@`)
+        await gitRepo.addRemote('authenticated', remoteUrlWithToken)
+        await gitRepo.push(['--set-upstream', 'authenticated', newBranch])
+        app.log.info('Pushed to ' + newBranch)
+
+        const privateKey = process.env.PRIVATE_KEY;
+        const appId = process.env.APP_ID;
+        const installationId = process.env.INSTALLATION_ID;
+
+        const auth = createAppAuth({
+          appId,
+          privateKey,
+          installationId,
+        });
+
+        const octokit = new Octokit({ auth });
+        // create a pull request
+        const pr = {
+          owner: 'kubevela',
+          repo: 'velad',
+          title: prTitle,
+          head: newBranch,
+          base: 'main',
+          body: prBody,
+        }
+
+        try {
+          await octokit.pulls.create(pr)
+          app.log.info('Created PR ' + prTitle)
+        } catch (error) {
+          console.error(`Error creating PR: ${error.message}`)
+          console.error(`Error status: ${error.status}`)
+        }
+
+      } catch (error) {
+        console.error('Error:', error)
+      }
+    }
+  })
+}
